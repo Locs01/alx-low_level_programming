896a3b52bd93bd9ecb12f5d92b39edb16930875f
Locs01 <sammierichie011@gmail.com>
2023-03-20 10:07:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Combination of single numbers
+ * Return: signle numbers and comma
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
