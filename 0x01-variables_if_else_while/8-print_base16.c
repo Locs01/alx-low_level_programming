d991c3dbe6050dbb26ed7ee6177364295774076e
Locs01 <sammierichie011@gmail.com>
2023-03-20 10:04:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Print Hex digits
+ * Return: Display base 16 decimals
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 22; i++)
+	{
+		if (i <= 9 || i >= 17)
+			putchar(tolower(i + '0'));
+	}
+	putchar('\n');
+	return  (0);
+}
