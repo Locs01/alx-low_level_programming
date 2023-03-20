0cf3ac9f1698ab8d61366c63efc3aaccd4c02193
Locs01 <sammierichie011@gmail.com>
2023-03-20 10:20:48 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Print 2 digit combinations
+ * Return: double combination
+*/
+int main(void)
+{
+	int i, m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (m = 0; m < 10 ; m++)
+	{
+		if (i < m && i != m)
+		{
+			putchar(i + '0');
+			putchar(m + '0');
+			if (i + m != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
