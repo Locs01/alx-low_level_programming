da7e3c92764bf33b2b6adb6dd89b57dc89e79cb4
Locs01 <sammierichie011@gmail.com>
2023-03-20 09:59:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Print lower case char in reverse
+ *
+ * Return: Print char in reverse
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
